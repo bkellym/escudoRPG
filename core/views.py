26b09dcca60b1f979d9b466e56cb0af72e690874
bkellym <bkelly.sm@gmail.com>
2020-05-28 20:03:36 -0300

    DEV03: Organiza CSS e get das fichas

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from core.models import ficha_vampiro
+from django.shortcuts import render, get_object_or_404
+from core.models import personagem_vampiro, ficha_vampiro
 
 # Create your views here.
-def ficha(request):
-    ficha = ficha_vampiro.objects.all()
-    return render(request, 'core/ficha.html', {'ficha': ficha})
+def ficha(request, pk):
+    personagem = get_object_or_404(personagem_vampiro, pk=pk)
+    ficha = ficha_vampiro.objects.get(id_personagem = personagem.pk)
+    return render(request, 'core/ficha.html', {'ficha': ficha, 'personagem': personagem})
