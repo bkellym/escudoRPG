bb62a3c1c901fb30669052bac23445b2438f3083
bkellym <bkelly.sm@gmail.com>
2020-05-29 16:18:21 -0300

    DEV02: Remodelagem das classes
    Descrição: Cria nova classe "Extra" para registrar itens da ficha que são personalizáveis.

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class personagem_vampiro(models.Model):
+class Personagem(models.Model):
     nome = models.CharField(max_length=200)
     idade = models.IntegerField(null=True, blank=True)
     natureza = models.CharField(max_length=150, null=True, blank=True)
@@ -11,50 +11,57 @@ class personagem_vampiro(models.Model):
     senhor = models.CharField(max_length=150, null=True, blank=True)
     conceito = models.CharField(max_length=150, null=True, blank=True)
 
-class ficha_vampiro(models.Model):
-    id_personagem = models.ForeignKey('personagem_vampiro', on_delete=models.CASCADE)
+class Ficha(models.Model):
+    id_personagem = models.ForeignKey('Personagem', on_delete=models.CASCADE)
 
     ############### Atributos ###############
-    forca = models.IntegerField(null=True, blank=True)
-    destreza = models.IntegerField(null=True, blank=True)
-    vigor = models.IntegerField(null=True, blank=True)
-    carisma = models.IntegerField(null=True, blank=True)
-    manipulacao = models.IntegerField(null=True, blank=True)
-    aparencia = models.IntegerField(null=True, blank=True)
-    percepcao = models.IntegerField(null=True, blank=True)
-    inteligencia = models.IntegerField(null=True, blank=True)
-    raciocinio = models.IntegerField(null=True, blank=True)
+    # Físicos
+    forca = models.IntegerField(null=True, blank=False, default=1)
+    destreza = models.IntegerField(null=True, blank=False, default=1)
+    vigor = models.IntegerField(null=True, blank=False, default=1)
+    # Sociais
+    carisma = models.IntegerField(null=True, blank=False, default=1)
+    manipulacao = models.IntegerField(null=True, blank=False, default=1)
+    aparencia = models.IntegerField(null=True, blank=False, default=1)
+    # Mentais
+    percepcao = models.IntegerField(null=True, blank=False, default=1)
+    inteligencia = models.IntegerField(null=True, blank=False, default=1)
+    raciocinio = models.IntegerField(null=True, blank=False, default=1)
 
     ############### Habilidades ###############
-    prontidao = models.IntegerField(null=True, blank=True)
-    esportes = models.IntegerField(null=True, blank=True)
-    briga = models.IntegerField(null=True, blank=True)
-    esquiva = models.IntegerField(null=True, blank=True)
-    empatia = models.IntegerField(null=True, blank=True)
-    expressao = models.IntegerField(null=True, blank=True)
-    intimidacao = models.IntegerField(null=True, blank=True)
-    lideranca = models.IntegerField(null=True, blank=True)
-    manha = models.IntegerField(null=True, blank=True)
-    empatia_animais = models.IntegerField(null=True, blank=True)
-    oficios = models.IntegerField(null=True, blank=True)
-    conducao = models.IntegerField(null=True, blank=True)
-    etiqueta = models.IntegerField(null=True, blank=True)
-    armas_fogo = models.IntegerField(null=True, blank=True)
-    armas_branca = models.IntegerField(null=True, blank=True)
-    performance = models.IntegerField(null=True, blank=True)
-    seguranca = models.IntegerField(null=True, blank=True)
-    furtividade = models.IntegerField(null=True, blank=True)
-    sobrevivencia = models.IntegerField(null=True, blank=True)
-    academicos = models.IntegerField(null=True, blank=True)
-    computador = models.IntegerField(null=True, blank=True)
-    financas = models.IntegerField(null=True, blank=True)
-    investigacao = models.IntegerField(null=True, blank=True)
-    direito = models.IntegerField(null=True, blank=True)
-    linguistica = models.IntegerField(null=True, blank=True)
-    medicina = models.IntegerField(null=True, blank=True)
-    ocultismo = models.IntegerField(null=True, blank=True)
-    politico = models.IntegerField(null=True, blank=True)
-    ciencia = models.IntegerField(null=True, blank=True)
+    # Talentos
+    prontidao = models.IntegerField(null=True, blank=False, default=0)
+    esportes = models.IntegerField(null=True, blank=False, default=0)
+    briga = models.IntegerField(null=True, blank=False, default=0)
+    esquiva = models.IntegerField(null=True, blank=False, default=0)
+    empatia = models.IntegerField(null=True, blank=False, default=0)
+    expressao = models.IntegerField(null=True, blank=False, default=0)
+    intimidacao = models.IntegerField(null=True, blank=False, default=0)
+    lideranca = models.IntegerField(null=True, blank=False, default=0)
+    manha = models.IntegerField(null=True, blank=False, default=0)
+    labia = models.IntegerField(null=True, blank=False, default=0)
+    # Perícias
+    empatia_animais = models.IntegerField(null=True, blank=False, default=0)
+    oficios = models.IntegerField(null=True, blank=False, default=0)
+    conducao = models.IntegerField(null=True, blank=False, default=0)
+    etiqueta = models.IntegerField(null=True, blank=False, default=0)
+    armas_fogo = models.IntegerField(null=True, blank=False, default=0)
+    armas_branca = models.IntegerField(null=True, blank=False, default=0)
+    performance = models.IntegerField(null=True, blank=False, default=0)
+    seguranca = models.IntegerField(null=True, blank=False, default=0)
+    furtividade = models.IntegerField(null=True, blank=False, default=0)
+    sobrevivencia = models.IntegerField(null=True, blank=False, default=0)
+    academicos = models.IntegerField(null=True, blank=False, default=0)
+    # Conhecimentos
+    computador = models.IntegerField(null=True, blank=False, default=0)
+    financas = models.IntegerField(null=True, blank=False, default=0)
+    investigacao = models.IntegerField(null=True, blank=False, default=0)
+    direito = models.IntegerField(null=True, blank=False, default=0)
+    linguistica = models.IntegerField(null=True, blank=False, default=0)
+    medicina = models.IntegerField(null=True, blank=False, default=0)
+    ocultismo = models.IntegerField(null=True, blank=False, default=0)
+    politico = models.IntegerField(null=True, blank=False, default=0)
+    ciencia = models.IntegerField(null=True, blank=False, default=0)
 
     ############### Virtudes ###############
     defesa = models.IntegerField(null=True, blank=True)
@@ -71,42 +78,16 @@ class ficha_vampiro(models.Model):
     vitalidade = models.IntegerField(null=True, blank=True)
     vitalidade_max = models.IntegerField(null=True, blank=True)
 
-    ############### Disciplinas e Antecendentes ###############
-    disciplina_1 = models.CharField(max_length=150, null=True, blank=True)
-    disciplina_valor_1 = models.IntegerField(null=True, blank=True)
-    disciplina_2 = models.CharField(max_length=150, null=True, blank=True)
-    disciplina_valor_2 = models.IntegerField(null=True, blank=True)
-    disciplina_3 = models.CharField(max_length=150, null=True, blank=True)
-    disciplina_valor_3 = models.IntegerField(null=True, blank=True)
-    disciplina_4 = models.CharField(max_length=150, null=True, blank=True)
-    disciplina_valor_4 = models.IntegerField(null=True, blank=True)
-    disciplina_5 = models.CharField(max_length=150, null=True, blank=True)
-    disciplina_valor_5 = models.IntegerField(null=True, blank=True)
-    disciplina_6 = models.CharField(max_length=150, null=True, blank=True)
-    disciplina_valor_6 = models.IntegerField(null=True, blank=True)
-    antecendentes_1 = models.CharField(max_length=150, null=True, blank=True)
-    antecendentes_valor_1 = models.IntegerField(null=True, blank=True)
-    antecendentes_2 = models.CharField(max_length=150, null=True, blank=True)
-    antecendentes_valor_2 = models.IntegerField(null=True, blank=True)
-    antecendentes_3 = models.CharField(max_length=150, null=True, blank=True)
-    antecendentes_valor_3 = models.IntegerField(null=True, blank=True)
-    antecendentes_4 = models.CharField(max_length=150, null=True, blank=True)
-    antecendentes_valor_4 = models.IntegerField(null=True, blank=True)
-    antecendentes_5 = models.CharField(max_length=150, null=True, blank=True)
-    antecendentes_valor_5 = models.IntegerField(null=True, blank=True)
-    antecendentes_6 = models.CharField(max_length=150, null=True, blank=True)
-    antecendentes_valor_6 = models.IntegerField(null=True, blank=True)
+class Extra(models.Model):
 
-    ############### Qualidades e Defeitos ###############
-    qualidade_1 = models.CharField(max_length=150, null=True, blank=True)
-    qualidade_2 = models.CharField(max_length=150, null=True, blank=True)
-    qualidade_3 = models.CharField(max_length=150, null=True, blank=True)
-    qualidade_4 = models.CharField(max_length=150, null=True, blank=True)
-    qualidade_5 = models.CharField(max_length=150, null=True, blank=True)
-    qualidade_6 = models.CharField(max_length=150, null=True, blank=True)
-    defeito_1 = models.CharField(max_length=150, null=True, blank=True)
-    defeito_2 = models.CharField(max_length=150, null=True, blank=True)
-    defeito_3 = models.CharField(max_length=150, null=True, blank=True)
-    defeito_4 = models.CharField(max_length=150, null=True, blank=True)
-    defeito_5 = models.CharField(max_length=150, null=True, blank=True)
-    defeito_6 = models.CharField(max_length=150, null=True, blank=True)
+    class Tipo(models.IntegerChoices):
+        DISCIPLINA = 1
+        ANTECENDENTE = 2
+        QUALIDADE = 3
+        DEFEITO = 4
+
+    id_personagem = models.ForeignKey('Personagem', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=150, null=True, blank=True)
+    descricao = models.CharField(max_length=500, null=True, blank=True)
+    valor = models.IntegerField(null=True, blank=True)
+    tipo = models.IntegerField(choices=Tipo.choices)
