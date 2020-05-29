327a87f45d5c8f89164d9937b335ce7d52dd7e4a
bkellym <bkelly.sm@gmail.com>
2020-05-29 16:18:46 -0300

    DEV03: Reestrutura objeto passado para tela de fichas.

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from core.models import personagem_vampiro, ficha_vampiro
+from core.models import *
 
 # Create your views here.
 def ficha(request, pk):
-    personagem = get_object_or_404(personagem_vampiro, pk=pk)
-    ficha = ficha_vampiro.objects.get(id_personagem = personagem.pk)
-    return render(request, 'core/ficha.html', {'ficha': ficha, 'personagem': personagem})
+    entidade = {}
+    personagem = get_object_or_404(Personagem, pk=pk)
+
+    entidade['personagem'] = personagem
+    entidade['ficha'] = Ficha.objects.get(id_personagem = personagem.pk)
+    entidade['disciplinas'] = Extra.objects.all().filter(id_personagem = personagem.pk, tipo = 1)
+    entidade['qualidades'] = Extra.objects.all().filter(id_personagem=personagem.pk, tipo=3)
+    entidade['defeitos'] = Extra.objects.all().filter(id_personagem=personagem.pk, tipo=4)
+
+    return render(request, 'core/ficha.html', {'entidade': entidade})
