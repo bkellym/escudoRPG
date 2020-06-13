a20500b736e487da81bde03897d3c495b25e18f7
bkellym <bkelly.sm@gmail.com>
2020-06-13 02:43:08 -0300

    Ajustes no Model para representar melhor as informações

--- core/models/model_descricao.py
+++ core/models/model_descricao.py
@@ -14,9 +14,14 @@ class Descricao(models.Model):
 
     titulo = models.CharField(max_length=150, null=True, blank=True)
     tipo = models.IntegerField(choices=Tipo.choices)
-    nivel = models.IntegerField(null=True, blank=True)
-    descricao = models.CharField(max_length=500, null=True, blank=True)
+    nivel = models.IntegerField(null=False, blank=False, default=0)
+    nome = models.CharField(max_length=150, null=True, blank=True)
+    descricao = models.TextField(max_length=500, null=True, blank=True)
+    observacao = models.TextField(max_length=500, null=True, blank=True)
+    sistema = models.TextField(max_length=500, null=True, blank=True)
+    teste = models.TextField(max_length=500, null=True, blank=True)
+    dificuldade = models.TextField(max_length=500, null=True, blank=True)
 
     def __str__(self):
-        return self.titulo + " | " + self.nivel
+        return self.titulo + " | " + str(self.nivel)
 
